8124c10242d7ad644cde3666db1bb591e3e7cd14
Nikita Lapkov <nikita.lapkov@gmail.com>
2018-02-05 04:17:46 +0300

    add link to file in admin

--- homework/admin.py
+++ homework/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import File, Subject, Material, Deadline
 
-admin.site.register([File, Subject, Material, Deadline])
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'download_url')
+    list_display_links = ('name',)
+
+    def download_url(self, obj):
+        return format_html('<a href="/media/{0}">Download {0}</a>', obj.name)
+    download_url.short_description = 'Storage link'
+
+
+admin.site.register([Subject, Material, Deadline])
